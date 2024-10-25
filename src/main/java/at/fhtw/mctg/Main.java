82a3e0a84ce3fc69e32004b6fbe76c41912c9ed4
Anzola Fabio <anzola.fabio@gmail.com>
2024-10-25 16:08:39 +0200

    Added Server init, router and first routes to main

--- src/main/java/at/fhtw/mctg/Main.java
+++ src/main/java/at/fhtw/mctg/Main.java
@@ -1,15 +1,31 @@
 package at.fhtw.mctg;
 
-import at.fhtw.httpserver.http.Method;
-import at.fhtw.httpserver.server.Request;
-import at.fhtw.sampleapp.service.weather.WeatherService;
+import at.fhtw.httpserver.server.Server;
+import at.fhtw.httpserver.utils.Router;
+import at.fhtw.mctg.service.echo.EchoService;
+import at.fhtw.mctg.service.users.UserService;
+import at.fhtw.mctg.service.weather.WeatherService;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-        WeatherService service = new WeatherService();
-        Request request = new Request();
-        request.setMethod(Method.GET);
-        System.out.println(service.handleRequest(request));
+        Server server = new Server(10001, configureRouter());
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static Router configureRouter()
+    {
+        Router router = new Router();
+        router.addService("/users", new UserService());
+        //router.addService("/weather", new WeatherService());
+        router.addService("/echo", new EchoService());
+
+        return router;
+    }
+
 }
