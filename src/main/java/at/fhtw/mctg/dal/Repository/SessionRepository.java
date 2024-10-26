30b6dfae6155ff5dd3e4b776cdadd06d012a7dad
Anzola Fabio <anzola.fabio@gmail.com>
2024-10-26 16:09:14 +0200

    Session route working

---
+++ src/main/java/at/fhtw/mctg/dal/Repository/SessionRepository.java
@@ -0,0 +1,72 @@
+package at.fhtw.mctg.dal.Repository;
+
+import at.fhtw.mctg.dal.DataAccessException;
+import at.fhtw.mctg.dal.UnitOfWork;
+import at.fhtw.mctg.model.Token;
+import at.fhtw.mctg.model.User;
+import at.fhtw.mctg.model.Weather;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class SessionRepository {
+    private UnitOfWork unitOfWork;
+
+    public SessionRepository(UnitOfWork unitOfWork) {
+        this.unitOfWork = unitOfWork;
+    }
+
+    public Collection<Token> getTokenByUsername(String username) {
+        try (PreparedStatement preparedStatement =
+                     this.unitOfWork.prepareStatement("""
+                                 select token.* from "token" 
+                                 join "user" 
+                                 on "token".fk_pk_user_id = "user".pk_user_id 
+                                 WHERE "user".username = ?;
+                             """)) {
+            preparedStatement.setString(1, username);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Collection<Token> tokenRows = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Token token = new Token(
+                        resultSet.getInt(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getInt(5));
+                tokenRows.add(token);
+            }
+
+            return tokenRows;
+        } catch (SQLException e) {
+            throw new DataAccessException("Select nicht erfolgreich", e);
+        }
+    }
+
+    public void createToken(int userId, String token) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        try (PreparedStatement preparedStatement = this.unitOfWork.prepareStatement(
+                """
+                        INSERT INTO "token" (token, created, expires, fk_pk_user_id) VALUES (?, ?, ?, ?)
+                        """)) {
+            preparedStatement.setString(1, token);
+            preparedStatement.setObject(2, currentDateTime);
+            preparedStatement.setObject(3, currentDateTime.plusDays(30L));
+            preparedStatement.setInt(4, userId);
+
+            preparedStatement.execute();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Insert not successful", e);
+        }
+    }
+}
---
+++ src/main/java/at/fhtw/mctg/service/session/SessionController.java
@@ -0,0 +1,110 @@
+package at.fhtw.mctg.service.session;
+
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.server.Request;
+import at.fhtw.httpserver.server.Response;
+import at.fhtw.mctg.controller.Controller;
+import at.fhtw.mctg.dal.Repository.SessionRepository;
+import at.fhtw.mctg.dal.Repository.UserRepository;
+import at.fhtw.mctg.dal.UnitOfWork;
+import at.fhtw.mctg.model.Session;
+import at.fhtw.mctg.model.Token;
+import at.fhtw.mctg.model.User;
+import at.fhtw.mctg.utils.JWTGenerator;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+
+public class SessionController extends Controller {
+
+    public Response createSession(Request request) {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        try (unitOfWork) {
+            Session input = this.getObjectMapper().readValue(request.getBody(), Session.class);
+
+            Collection<User> dbUsers = new UserRepository(unitOfWork).getUserByName(input.getUsername());
+
+            if (dbUsers.isEmpty()) {
+                return new Response(
+                        HttpStatus.UNAUTHORIZED,
+                        ContentType.JSON,
+                        "{ \"message\" : \"User not found\" }"
+                );
+            }
+
+            User user = ((ArrayList<User>) dbUsers).get(0);
+
+            if (!user.getPassword().contentEquals(input.getPassword())) {
+                return new Response(
+                        HttpStatus.UNAUTHORIZED,
+                        ContentType.JSON,
+                        "{ \"message\" : \"Password doesn't match\" }"
+                );
+            }
+
+            // Check if there is a non expired token
+            Collection<Token> tokens = new SessionRepository(unitOfWork).getTokenByUsername(user.getUsername());
+
+            if (!tokens.isEmpty()) {
+                // Get latest token
+                Token latestToken = ((ArrayList<Token>) tokens).get(tokens.size()-1);
+
+                // Get expire date
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                LocalDateTime exp = LocalDateTime.parse(latestToken.getExpires().substring(0,19), formatter);
+
+                if (exp.isAfter(LocalDateTime.now())) {
+                    // Token is still valid
+                    return new Response(
+                            HttpStatus.OK,
+                            ContentType.JSON,
+                            this.getObjectMapper().writeValueAsString(latestToken)
+                    );
+                }
+            }
+
+            // No token yet or expired
+            // Create token
+            String header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+            String payload = "{\"user\":"+user.getUsername()+",\"role\":"+user.getUserId()+"}";
+            String jwtToken = JWTGenerator.createJWT(header, payload);
+
+            // save token to db
+            new SessionRepository(unitOfWork).createToken(user.getUserId(), jwtToken);
+
+            // retrieve latest token
+            tokens = new SessionRepository(unitOfWork).getTokenByUsername(user.getUsername());
+            Token latestToken = ((ArrayList<Token>) tokens).get(tokens.size()-1);
+
+            // Commit work
+            unitOfWork.commitTransaction();
+
+            // return token
+            return new Response(
+                    HttpStatus.OK,
+                    ContentType.JSON,
+                    this.getObjectMapper().writeValueAsString(latestToken)
+            );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            unitOfWork.rollbackTransaction();
+            return new Response(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    ContentType.JSON,
+                    "{ \"message\" : \"Internal Server Error\" }"
+            );
+        }
+    }
+
+}
