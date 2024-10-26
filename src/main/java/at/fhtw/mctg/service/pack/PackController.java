41d112fefc167f6d1b0ec93ad34e637b828886ff
Anzola Fabio <anzola.fabio@gmail.com>
2024-10-26 16:56:41 +0200

    Added pack controller and service

---
+++ src/main/java/at/fhtw/mctg/service/pack/PackController.java
@@ -0,0 +1,6 @@
+package at.fhtw.mctg.service.pack;
+
+import at.fhtw.mctg.controller.Controller;
+
+public class PackController extends Controller {
+}
---
+++ src/main/java/at/fhtw/mctg/service/pack/PackService.java
@@ -0,0 +1,32 @@
+package at.fhtw.mctg.service.pack;
+
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.http.Method;
+import at.fhtw.httpserver.server.Request;
+import at.fhtw.httpserver.server.Response;
+import at.fhtw.httpserver.server.Service;
+
+public class PackService implements Service {
+    private final PackController packController;
+
+    public PackService() {
+        this.packController = new PackController();
+    }
+
+    @Override
+    public Response handleRequest(Request request) {
+
+        // POST
+        if (request.getMethod() == Method.POST) {
+            // TODO: Implement this method
+            return null;
+        }
+
+        return new Response(
+                HttpStatus.BAD_REQUEST,
+                ContentType.JSON,
+                "[]"
+        );
+    }
+}
