b1530c3772ad4011d1c4ddd5f3963cb88ab1d806
Anzola Fabio <anzola.fabio@gmail.com>
2024-10-26 16:56:30 +0200

    Added card controller and service

---
+++ src/main/java/at/fhtw/mctg/service/card/CardController.java
@@ -0,0 +1,6 @@
+package at.fhtw.mctg.service.card;
+
+import at.fhtw.mctg.controller.Controller;
+
+public class CardController extends Controller {
+}
---
+++ src/main/java/at/fhtw/mctg/service/card/CardService.java
@@ -0,0 +1,32 @@
+package at.fhtw.mctg.service.card;
+
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.http.Method;
+import at.fhtw.httpserver.server.Request;
+import at.fhtw.httpserver.server.Response;
+import at.fhtw.httpserver.server.Service;
+
+public class CardService implements Service {
+    private final CardController cardController;
+
+    public CardService() {
+        this.cardController = new CardController();
+    }
+
+    @Override
+    public Response handleRequest(Request request) {
+
+        // GET
+        if (request.getMethod() == Method.GET) {
+            // TODO: Implement this method
+            return null;
+        }
+
+        return new Response(
+                HttpStatus.BAD_REQUEST,
+                ContentType.JSON,
+                "[]"
+        );
+    }
+}
