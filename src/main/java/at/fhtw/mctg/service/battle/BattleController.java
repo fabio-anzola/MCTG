d4ac56394457f32328ad1ce5f04ae67edca2f7ed
Anzola Fabio <anzola.fabio@gmail.com>
2024-10-26 16:56:25 +0200

    Added battle controller and service

---
+++ src/main/java/at/fhtw/mctg/service/battle/BattleController.java
@@ -0,0 +1,6 @@
+package at.fhtw.mctg.service.battle;
+
+import at.fhtw.mctg.controller.Controller;
+
+public class BattleController extends Controller {
+}
---
+++ src/main/java/at/fhtw/mctg/service/battle/BattleService.java
@@ -0,0 +1,33 @@
+package at.fhtw.mctg.service.battle;
+
+import at.fhtw.httpserver.http.ContentType;
+import at.fhtw.httpserver.http.HttpStatus;
+import at.fhtw.httpserver.http.Method;
+import at.fhtw.httpserver.server.Request;
+import at.fhtw.httpserver.server.Response;
+import at.fhtw.httpserver.server.Service;
+import at.fhtw.mctg.service.session.SessionController;
+
+public class BattleService implements Service {
+    private final BattleController battleController;
+
+    public BattleService() {
+        this.battleController = new BattleController();
+    }
+
+    @Override
+    public Response handleRequest(Request request) {
+
+        // POST
+        if (request.getMethod() == Method.POST) {
+            // TODO: Implement this method
+            return null;
+        }
+
+        return new Response(
+                HttpStatus.BAD_REQUEST,
+                ContentType.JSON,
+                "[]"
+        );
+    }
+}
