8b653c58965ec5d101b25b4b7abed4302198f96b
Anzola Fabio <anzola.fabio@gmail.com>
2025-01-02 16:28:27 +0100

    Added stats model

---
+++ src/main/java/at/fhtw/mctg/model/Stats.java
@@ -0,0 +1,50 @@
+package at.fhtw.mctg.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Getter;
+
+@Getter
+public class Stats {
+    @JsonAlias({"Username"})
+    private String username;
+
+    @JsonAlias({"Name"})
+    private String name;
+
+    @JsonAlias({"Elo"})
+    private int elo;
+
+    @JsonAlias({"Wins"})
+    private int wins;
+
+    @JsonAlias({"Losses"})
+    private int losses;
+
+    @JsonAlias({"Ties"})
+    private int ties;
+
+    /**
+     * Default Constructor needed by Lombok
+     */
+    public Stats() {
+    }
+
+    /**
+     * Constructor to set all args
+     *
+     * @param username username
+     * @param name user set name
+     * @param elo elo int
+     * @param wins wins int
+     * @param losses loss int
+     * @param ties ties int
+     */
+    public Stats(String username, String name, int elo, int wins, int losses, int ties) {
+        this.username = username;
+        this.name = name;
+        this.elo = elo;
+        this.wins = wins;
+        this.losses = losses;
+        this.ties = ties;
+    }
+}
