8931de0ca293a43ccc30294ecd10fc3e6e021653
Anzola Fabio <anzola.fabio@gmail.com>
2025-01-05 17:09:58 +0100

    Init battle service

--- src/main/java/at/fhtw/mctg/service/battle/BattleService.java
+++ src/main/java/at/fhtw/mctg/service/battle/BattleService.java
@@ -7,6 +7,8 @@ import at.fhtw.httpserver.server.Request;
 import at.fhtw.httpserver.server.Response;
 import at.fhtw.httpserver.server.Service;
 import at.fhtw.mctg.controller.battle.BattleController;
+import at.fhtw.mctg.dal.Repository.BattleRepository;
+import at.fhtw.mctg.model.Battle;
 
 public class BattleService implements Service {
     private final BattleController battleController;
@@ -20,8 +22,27 @@ public class BattleService implements Service {
 
         // POST
         if (request.getMethod() == Method.POST) {
-            // TODO: Implement this method
-            return null;
+            if (request.getHeaderMap().getHeader("Authorization") == null) {
+                return new Response(
+                        HttpStatus.UNAUTHORIZED,
+                        ContentType.JSON,
+                        "{ \"message\" : \"Access token is missing or invalid\" }"
+                );
+            }
+
+            Battle battle = battleController.initBattle(request);
+
+            if (battle == null) {
+                return new Response(
+                        HttpStatus.BAD_REQUEST,
+                        ContentType.JSON,
+                        "[]"
+                );
+            }
+
+            battleController.waitForBattle(battle); // blocking!
+
+            return battleController.getLog(battle);
         }
 
         return new Response(
