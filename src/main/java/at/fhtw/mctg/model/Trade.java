3bf2cee8dcfe64fd3054aae9851136436ba77e9b
Anzola Fabio <anzola.fabio@gmail.com>
2025-01-06 16:28:36 +0100

    Adjusted Trade Model

--- src/main/java/at/fhtw/mctg/model/Trade.java
+++ src/main/java/at/fhtw/mctg/model/Trade.java
@@ -2,28 +2,32 @@ package at.fhtw.mctg.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
 @Getter
 public class Trade {
 
-    @JsonAlias({"tradeId"})
-    private int tradeId;
+    @JsonAlias({"Id"})
+    private String tradeId;
 
     @JsonAlias({"initiatorId"})
+    @Setter
     private int initiatorId;
 
     @JsonAlias({"partnerId"})
     private int partnerId;
 
-    @JsonAlias({"senderCardId"})
+    @JsonAlias({"CardToTrade"})
+    @Getter
     private String senderCardId;
 
     @JsonAlias({"receiverCardId"})
     private String receiverCardId;
 
     @JsonAlias({"status"})
+    @Setter
     private TradeStatus status;
 
     @JsonAlias({"timeCreated"})
@@ -32,13 +36,13 @@ public class Trade {
     @JsonAlias({"timeCompleted"})
     private Timestamp timeCompleted;
 
-    @JsonAlias({"requestedType"})
+    @JsonAlias({"Type"})
     private CardType requestedType;
 
-    @JsonAlias({"requestedDamage"})
+    @JsonAlias({"MinimumDamage"})
     private int requestedDamage;
 
-    public Trade(int tradeId, int initiatorId, int partnerId, String senderCardId, String receiverCardId, TradeStatus status, Timestamp timeCreated, Timestamp timeCompleted, CardType requestedType, int requestedDamage) {
+    public Trade(String tradeId, int initiatorId, int partnerId, String senderCardId, String receiverCardId, TradeStatus status, Timestamp timeCreated, Timestamp timeCompleted, CardType requestedType, int requestedDamage) {
         this.tradeId = tradeId;
         this.initiatorId = initiatorId;
         this.partnerId = partnerId;
