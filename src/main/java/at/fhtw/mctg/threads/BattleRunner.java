9e9efea69cbdf6f6fb964618df0299b0e00b91cb
Anzola Fabio <anzola.fabio@gmail.com>
2025-01-10 15:58:55 +0100

    Added docs to runner

--- src/main/java/at/fhtw/mctg/threads/BattleRunner.java
+++ src/main/java/at/fhtw/mctg/threads/BattleRunner.java
@@ -9,29 +9,70 @@ import at.fhtw.mctg.model.*;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Random;
 
+/**
+ * This class represents the task for managing and executing battles between two users
+ * in a card game. It implements the Runnable interface to allow battles to be processed
+ * in a separate thread.
+ * <p>
+ * The `BattleRunner` handles the complete battle logic, including:
+ * - Initializing the battle state and variables.
+ * - Logging battle-related events and rounds.
+ * - Executing battle rounds, where user cards are selected and combat is determined.
+ * - Applying game-specific rules and mechanics for card interactions.
+ * - Determining the winner of each round.
+ * - Ensuring the battle runs for a maximum number of defined rounds or until one user's deck is empty.
+ */
 public class BattleRunner implements Runnable {
 
+    /**
+     * Instance of the BattleController class to manage battle-related operations.
+     */
     BattleController battleController;
 
+    /**
+     * Represents the current battle.
+     */
     Battle battle;
 
+    /**
+     * Represents the first user (User A) participating in a battle.
+     */
     User userA;
 
+    /**
+     * Represents the second User participating in the battle.
+     */
     User userB;
 
+    /**
+     * Represents the deck of cards belonging to User A in the context of a battle.
+     */
     private ArrayList<Card> deckUserA;
 
+    /**
+     * Represents the deck of cards belonging to User B in the context of a battle.
+     */
     private ArrayList<Card> deckUserB;
 
+    /**
+     * Defines the maximum number of battle rounds allowed in a game.
+     */
     final int MAX_ROUNDS = 100;
 
-    private Random random;
+    /**
+     * An instance of the Random class used for generating random values during the execution of the BattleRunner class.
+     */
+    private final Random random;
 
+    /**
+     * Constructs a new BattleRunner instance to manage and run a battle.
+     *
+     * @param battle the Battle instance representing the battle to be managed,
+     *               containing details such as participants, rounds, and unique identifiers.
+     */
     public BattleRunner(Battle battle) {
-        super();
         this.battle = battle;
         this.battleController = new BattleController();
         this.random = new Random();
@@ -39,6 +80,23 @@ public class BattleRunner implements Runnable {
         initVars();
     }
 
+    /**
+     * Executes the battle logic for a defined number of rounds or until one player's deck is empty.
+     * The method handles the initialization, execution of battle rounds, applying custom rules,
+     * specifying battle mechanics, and determining the winner based on card damages.
+     * <p>
+     * Key actions performed by this method:
+     * - Logs the starting details of the battle, including usernames of participants.
+     * - Sets the battle start time and ensures proper transactional handling.
+     * - Executes individual rounds with rules determining interactions between cards (e.g., special abilities and elemental type advantages).
+     * - Applies predefined custom rules based on specific conditions within the game.
+     * - Logs detailed battle progress, including card selections, applied rules, and round outcomes.
+     * - Processes card exchanges between players' decks based on win/loss outcomes of rounds.
+     * - Handles the termination of the battle either by reaching the maximum number of rounds or by deck depletion.
+     * - Records comprehensive logs of every significant battle event and commits changes within transactions.
+     * <p>
+     * Throws runtime exceptions for any issues during the transaction close process.
+     */
     @Override
     public void run() {
         UnitOfWork unitOfWork = new UnitOfWork();
@@ -85,42 +143,42 @@ public class BattleRunner implements Runnable {
             }
 
             // Special rules
-            if ((userA_card.getName() == "Goblin" && userB_card.getName() == "Dragon")) {
+            if (("Goblin".equals(userA_card.getName()) && "Dragon".equals(userB_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Goblin is too afraid to attack!");
                 damageA = 0;
-            } else if ((userB_card.getName() == "Goblin" && userA_card.getName() == "Dragon")) {
+            } else if (("Goblin".equals(userB_card.getName()) && "Dragon".equals(userA_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Goblin is too afraid to attack!");
                 damageB = 0;
             }
 
-            if ((userA_card.getName() == "Wizard" && userB_card.getName() == "Ork")) {
+            if (("Wizard".equals(userA_card.getName()) && "Ork".equals(userB_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Wizard controls Ork. No damage!");
                 damageB = 0;
-            } else if ((userB_card.getName() == "Ork" && userA_card.getName() == "Wizard")) {
+            } else if (("Ork".equals(userB_card.getName()) && "Wizard".equals(userA_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Wizard controls Ork. No damage!");
                 damageA = 0;
             }
 
-            if ((userA_card.getName() == "Knight" && (userB_card.getType() == CardType.SPELL && userB_card.getElement() == Elements.WATER))) {
+            if (("Knight".equals(userA_card.getName()) && (userB_card.getType() == CardType.SPELL && userB_card.getElement() == Elements.WATER))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Knight drowns due to WaterSpell!");
                 damageA = 0;
-            } else if ((userB_card.getName() == "Knight" && (userA_card.getType() == CardType.SPELL && userA_card.getElement() == Elements.WATER))) {
+            } else if (("Knight".equals(userB_card.getName()) && (userA_card.getType() == CardType.SPELL && userA_card.getElement() == Elements.WATER))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Knight drowns due to WaterSpell!");
                 damageB = 0;
             }
 
-            if ((userA_card.getName() == "Kraken" && userB_card.getType() == CardType.SPELL)) {
+            if (("Kraken".equals(userA_card.getName()) && userB_card.getType() == CardType.SPELL)) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Kraken is immune to spells!");
                 damageB = 0;
-            } else if ((userB_card.getName() == "Kraken" && userA_card.getType() == CardType.SPELL)) {
+            } else if (("Kraken".equals(userB_card.getName()) && userA_card.getType() == CardType.SPELL)) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Kraken is immune to spells!");
                 damageA = 0;
             }
 
-            if ((userA_card.getName() == "FireElf" && userB_card.getName() == "Dragon")) {
+            if (("FireElf".equals(userA_card.getName()) && "Dragon".equals(userB_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> FireElf evades Dragon's attack!");
                 damageB = 0;
-            } else if ((userB_card.getName() == "FireElf" && userA_card.getName() == "Dragon")) {
+            } else if (("FireElf".equals(userB_card.getName()) && "Dragon".equals(userA_card.getName()))) {
                 new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> FireElf evades Dragon's attack!");
                 damageA = 0;
             }
@@ -132,28 +190,28 @@ public class BattleRunner implements Runnable {
                 // water -> fire
                 if (userA_card.getElement() == Elements.WATER && userB_card.getElement() == Elements.FIRE) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Fire > Water");
-                    damageA = 2*damageA;
+                    damageA = 2 * damageA;
                 } else if (userB_card.getElement() == Elements.WATER && userA_card.getElement() == Elements.FIRE) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Fire > Water");
-                    damageB = 2*damageB;
+                    damageB = 2 * damageB;
                 }
 
                 // fire -> normal
                 if (userA_card.getElement() == Elements.FIRE && userB_card.getElement() == Elements.NORMAL) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Fire > Normal");
-                    damageA = 2*damageA;
+                    damageA = 2 * damageA;
                 } else if (userB_card.getElement() == Elements.FIRE && userA_card.getElement() == Elements.NORMAL) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Fire > Normal");
-                    damageB = 2*damageB;
+                    damageB = 2 * damageB;
                 }
 
                 // normal -> water
                 if (userA_card.getElement() == Elements.NORMAL && userB_card.getElement() == Elements.WATER) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "->  Normal > Water");
-                    damageA = 2*damageA;
+                    damageA = 2 * damageA;
                 } else if (userB_card.getElement() == Elements.NORMAL && userA_card.getElement() == Elements.WATER) {
                     new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "-> Normal > Water");
-                    damageB = 2*damageB;
+                    damageB = 2 * damageB;
                 }
             }
 
@@ -185,47 +243,65 @@ public class BattleRunner implements Runnable {
         }
 
         // Overall winner
-        unitOfWork = new UnitOfWork();
+        UnitOfWork unitOfWorkOverall = new UnitOfWork();
+        try (unitOfWorkOverall) {
 
-        new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "### Rounds complete ###");
-        if (deckUserA.isEmpty()) {
-            new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "User B (" + (userB.getUsername()) + ") wins!");
+            new BattleRepository(unitOfWorkOverall).addLogLine(this.battle.getBattleId(), "### Rounds complete ###");
+            if (deckUserA.isEmpty()) {
+                new BattleRepository(unitOfWorkOverall).addLogLine(this.battle.getBattleId(), "User B (" + (userB.getUsername()) + ") wins!");
 
-            userA.setElo(userA.getElo() - 5);
-            userB.setElo(userB.getElo() + 3);
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.LOSS);
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.WIN);
-        } else if (deckUserB.isEmpty()) {
-            new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "User A (" + (userA.getUsername()) + ") wins!");
+                userA.setElo(userA.getElo() - 5);
+                userB.setElo(userB.getElo() + 3);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.LOSS);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.WIN);
+            } else if (deckUserB.isEmpty()) {
+                new BattleRepository(unitOfWorkOverall).addLogLine(this.battle.getBattleId(), "User A (" + (userA.getUsername()) + ") wins!");
 
-            userA.setElo(userA.getElo() + 3);
-            userB.setElo(userB.getElo() - 5);
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.LOSS);
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.WIN);
-        } else {
-            new BattleRepository(unitOfWork).addLogLine(this.battle.getBattleId(), "Battle is a draw!");
+                userA.setElo(userA.getElo() + 3);
+                userB.setElo(userB.getElo() - 5);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.LOSS);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.WIN);
+            } else {
+                new BattleRepository(unitOfWorkOverall).addLogLine(this.battle.getBattleId(), "Battle is a draw!");
 
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.TIE);
-            new BattleRepository(unitOfWork).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.TIE);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userA.getUserId(), BattleStatus.TIE);
+                new BattleRepository(unitOfWorkOverall).finalizeUserBattle(battle.getBattleId(), userB.getUserId(), BattleStatus.TIE);
+            }
+
+            // set battle infos
+            new BattleRepository(unitOfWorkOverall).finalizeBattle(battle.getBattleId(), new Timestamp(System.currentTimeMillis()), round);
+
+            // set user stats
+            // ELO
+            // +3 win
+            // -5 loss
+            new UserRepository(unitOfWorkOverall).updateUserByName(userA.getUsername(), userA);
+            new UserRepository(unitOfWorkOverall).updateUserByName(userA.getUsername(), userB);
+
+            unitOfWorkOverall.commitTransaction();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            unitOfWorkOverall.rollbackTransaction();
         }
-
-        // set battle infos
-        new BattleRepository(unitOfWork).finalizeBattle(battle.getBattleId(), new Timestamp(System.currentTimeMillis()), round);
-
-        // set user stats
-        // ELO
-        // +3 win
-        // -5 loss
-        new UserRepository(unitOfWork).updateUserByName(userA.getUsername(), userA);
-        new UserRepository(unitOfWork).updateUserByName(userA.getUsername(), userB);
-
-        unitOfWork.commitTransaction();
     }
 
+    /**
+     * Initializes battle-related variables required for executing the battle logic.
+     *
+     * This method performs the following operations:
+     * 1. Instantiates a new UnitOfWork object to manage database transactions.
+     * 2. Retrieves a list of UserBattle objects representing the participants of the battle.
+     * 3. Initializes the two users involved in the battle using their user IDs.
+     * 4. Retrieves and initializes the card decks for both users.
+     * 5. Commits the current transaction to persist changes made during initialization.
+     */
     private void initVars() {
         UnitOfWork unitOfWork = new UnitOfWork();
+        try (unitOfWork) {
+
         ArrayList<UserBattle> users = (ArrayList<UserBattle>) new BattleRepository(unitOfWork).getBattleUsers(this.battle.getBattleId());
-        System.out.println(users.size());
 
         // Init users
         this.userA = new UserRepository(unitOfWork).getUserById(users.get(0).getUserId());
@@ -236,5 +312,10 @@ public class BattleRunner implements Runnable {
         this.deckUserB = (ArrayList<Card>) new CardRepository(unitOfWork).getCardsByUserId(this.userB.getUserId());
 
         unitOfWork.commitTransaction();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            unitOfWork.rollbackTransaction();
+        }
     }
 }
